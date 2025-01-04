f144af179bbd1e47a0546ec1d5ee8d87f308d99e
Gopu-k09 <gopukrishnan0459@gmail.com>
2025-01-04 23:38:38 +0530

    updated

--- gnotes/settings.py
+++ gnotes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-57*13u@!@7%0mf+cdwedypm#ck+71p4*z^9gn(pjw0lbj0&&f5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #True
 
 ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['gnotes.onrender.com', '127.0.0.1']
 
 
 
@@ -119,7 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
