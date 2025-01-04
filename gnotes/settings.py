a96cbb3aaf318ed341c6f12a2b6d05c589ec7068
Gopu-k09 <gopukrishnan0459@gmail.com>
2025-01-05 00:05:14 +0530

    updated

--- gnotes/settings.py
+++ gnotes/settings.py
@@ -12,6 +12,19 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+import dj_database_url
+
+load_dotenv()  # Load environment variables from .env file
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG') == 'False'  # Ensure DEBUG is a boolean
+
+
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +33,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-57*13u@!@7%0mf+cdwedypm#ck+71p4*z^9gn(pjw0lbj0&&f5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False #True
+# DEBUG = False #True
 
 ALLOWED_HOSTS = []
 
@@ -80,12 +91,19 @@ WSGI_APPLICATION = 'gnotes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
+
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+# }
 
 
 # Password validation
